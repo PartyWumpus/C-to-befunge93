45bdc5258db50712329bcd4294f574e388340636
PartyWumpus <48649272+PartyWumpus@users.noreply.github.com>
2025-04-02 00:37:50 +0100

    Use mold linker for debug & format flake

--- flake.nix
+++ flake.nix
@@ -1,31 +1,36 @@
 {
-	inputs = {
-		flake-utils.url = "github:numtide/flake-utils";
-		nixpkgs.url = "github:nixos/nixpkgs";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs";
 
-		rust-overlay.url = "github:oxalica/rust-overlay";
-	};
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
 
-	outputs = {
-		self,
-		nixpkgs,
-		flake-utils,
-		rust-overlay,
-		...
-	}:
-		flake-utils.lib.eachDefaultSystem (system:
-			let
-				pkgs = import nixpkgs {
-					inherit system;
-					overlays = [ (import rust-overlay) ];
-				};
-			in with pkgs; rec {
-				devShell = mkShell rec {
-					packages = [
-						rust-bin.stable.latest.default
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
+    in
+      with pkgs; rec {
+        formatter = pkgs.alejandra;
+        devShell = mkShell rec {
+          packages = [
+            (rust-bin.selectLatestNightlyWith (toolchain:
+              toolchain.default.override {
+                #extensions = [ "rustc-codegen-cranelift-preview" ];
+              }))
             gcc13
+            mold
             python3
-					];
-				};
-			});
+          ];
+        };
+      });
 }
