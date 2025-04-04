9643e6de3902771479d6fea0ca85b3ef988fc331
PartyWumpus <48649272+PartyWumpus@users.noreply.github.com>
2025-04-04 03:16:45 +0100

    Make tests run in parallel so they finish faster

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-import json, subprocess, sys, re, os, time
+from enum import IntEnum
+import json, subprocess, sys, re, os, asyncio, tempfile
+# TODO: consider returning the tracebacks n stuff for each test
 
 compiler = "./target/release/c-to-befunge"
-# TODO: fix the path
 befunge_interpeter = "./RBeJ/target/release/rbej"
 befunge_out_path = "out.b93"
 
@@ -14,8 +15,12 @@ successes = []
 
 accepted_invalid_code = []
 
-valid_tests = {}
-invalid_tests = []
+valid_tests: dict[str, str] = {}
+invalid_tests: list[str] = []
+
+scores = [0,0,0]
+
+NUM_CORES = 7
 
 chapter_regex = ".*chapter_(1|2|3|4|5|6|7|8|9|10)/.*"
 
@@ -39,64 +44,128 @@ if len(sys.argv) > 1:
     exit(0)
 
 # compile compiler
-subprocess.run(["cargo", "build", "--release"])
+subprocess.run(["cargo", "build", "--release"], capture_output=True)
+
+class TestType(IntEnum):
+    Invalid = 0
+    Valid = 1
+
+class Status(IntEnum):
+    RED = 0
+    YELLOW = 1
+    GREEN = 2
+
+def status_to_code(status: Status):
+    match status:
+        case Status.RED:
+            return "\033[1;31m"
+        case Status.YELLOW:
+            return "\033[0;33m"
+        case Status.GREEN:
+            return "\033[1;32m"
 
 # run tests
-print("\033[0;35m\nINVALID C TESTS")
-time.sleep(1)
+async def test_invalid(test: str) -> tuple[str, Status]:
+    with tempfile.NamedTemporaryFile() as tf:
+        proc = await asyncio.create_subprocess_shell(f"{compiler} {test} -q -o {tf.name}", stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+        await proc.communicate()
+        if proc.returncode != 0: # compiler failed, which is what we want
+            successes.append(test)
+            return (f"PASS {test}", Status.GREEN)
+        else:
+            accepted_invalid_code.append(test)
+            return (f"INVALID ACCEPTED {test}", Status.YELLOW)
 
-for test in invalid_tests:
-    print(f"\033[0m{test}", end="");
-    sys.stdout.flush();
-    res = subprocess.run([compiler, test, "-s", "-o", befunge_out_path])
-    if res.returncode != 0: # compiler failed
-        print(f"\r\033[1;32mPASS {test}")
+async def test_valid(test: str) -> tuple[str, Status]:
+    with tempfile.NamedTemporaryFile() as tf:
+        proc = await asyncio.create_subprocess_shell(f"{compiler} {test} -q -o {tf.name}", stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+        await proc.communicate()
+        if proc.returncode != 0: # compiler failed
+            compile_fails.append(test)
+            return (f"FAIL COMPILATION {test}", Status.RED)
+
+        proc = await asyncio.create_subprocess_shell(f"{befunge_interpeter} {tf.name}",stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+        try:
+
+            stdout, stderr = await asyncio.wait_for(proc.communicate(), 5)
+        except asyncio.TimeoutError:
+            proc.kill()
+            interpreter_crashes.append(test)
+            return (f"INTERPRETER TIMEOUT {test}", Status.YELLOW)
+
+        if proc.returncode != 0: # interpeter crashed
+            interpreter_crashes.append(test)
+            return (f"INTERPRETER CRASH {test}", Status.YELLOW)
+
+        if (int(stdout.splitlines()[-1]) % 256 != valid_tests[test]):
+            incorrect_execution.append(test)
+            return (f"FAIL EXECUTION (got: {stdout.splitlines()[-1]} expected: {valid_tests[test]}) {test}", Status.RED)
+
         successes.append(test)
-    else:
-        print(f"\r\033[1;33mINVALID ACCEPTED {test}")
-        accepted_invalid_code.append(test)
+        return (f"PASS {test}", Status.GREEN)
 
-print("\033[0;35m\n\nVALID C TESTS")
-time.sleep(0.5)
-for test in valid_tests:
-    print(f"\033[0m{test}", end="");
-    sys.stdout.flush();
-    res = subprocess.run([compiler, test, "-q", "-o", befunge_out_path])
-    if res.returncode != 0: # compiler failed
-        compile_fails.append(test)
-        print(f"\r\033[1;33mFAIL COMPILATION {test}")
-        continue
+def move_cursor_up(n: int):
+    print("\033[F"*n)
 
-    try:
-        res = subprocess.run([befunge_interpeter, befunge_out_path],capture_output=True,text=True,timeout=3)
-    except subprocess.TimeoutExpired:
-        interpreter_crashes.append(test)
-        print(f"\r\033[1;33mINTERPRETER TIMEOUT {test}")
-        continue
-    if res.returncode != 0: # interpeter crashed
-        interpreter_crashes.append(test)
-        print(f"\r\033[1;33mINTERPRETER CRASH {test}")
-        continue
+in_progress_tests = ["... waiting"]*NUM_CORES
+async def test_runner(tsts: list[tuple[TestType, str]], n:int):
+    if n == 0:
+          print("\n"*NUM_CORES)
+    while True:
+        if len(tsts) > 0:
+            test = tsts.pop()
+        else:
+            break
+        in_progress_tests[n] = test[1]
+        if test[0] == TestType.Valid:
+            res = await test_valid(test[1])
+        else:
+            res = await test_invalid(test[1])
+        move_cursor_up(NUM_CORES+2)
+        scores[res[1]] += 1
+        if res[1] != Status.GREEN:
+            sys.stdout.write(status_to_code(res[1]))
+            sys.stdout.write(res[0])
+            sys.stdout.write("\033[0m") # reset
+            sys.stdout.write("\n")
+        sys.stdout.write("\33[2K\r")
+        sys.stdout.write(f"{scores[0] + scores[1] + scores[2]}/{len(valid_tests) + len(invalid_tests)}")
 
-    if (int(res.stdout.splitlines()[-1]) % 256 != valid_tests[test]):
-        incorrect_execution.append(test)
-        print(f"\r\033[1;31mFAIL EXECUTION (got: {res.stdout} expected: {valid_tests[test]}) {test}")
-    else:
-        print(f"\r\033[1;32mPASS {test}")
-        successes.append(test)
+        for i in Status:
+            sys.stdout.write(status_to_code(i))
+            sys.stdout.write(f" {scores[i]}")
+        sys.stdout.write("\033[0m") # reset
+        sys.stdout.write("\n")
+        for in_prog in in_progress_tests:
+            sys.stdout.write("\33[2K\r")
+            sys.stdout.write(f"{in_prog}\n")
+        sys.stdout.flush()
+    in_progress_tests[n] = "done"
+
+async def run_tests():
+    tasks = []
+    tests: list[tuple[TestType, str]] = list(map(lambda x: (TestType.Valid, x), valid_tests)) + list(map(lambda x: (TestType.Invalid, x), invalid_tests))
+    for i in range(NUM_CORES):
+        tasks.append(test_runner(tests, i))
+    res = await asyncio.gather(*tasks, return_exceptions=True)
+    sys.stdout.write("\n")
+    for i, err in enumerate(res):
+        if err != None:
+            print(err)
+    sys.stdout.flush()
 
 
+asyncio.run(run_tests())
 print("\033[0m")
-
-print(f"compile failures: {compile_fails}\n")
-print(f"incorrect execution: {incorrect_execution}\n")
-print(f"interpreter crashes: {interpreter_crashes}\n")
-print(f"invalid code acceptances: {accepted_invalid_code}\n")
 
 total_tests = len(successes) + len(interpreter_crashes) + len(compile_fails) + len(accepted_invalid_code) + len(incorrect_execution)
 
+
+print(f"\033[1;33minvalid acceptances: {len(accepted_invalid_code)}\033[0m")
 print(f"\033[1;33minterpreter crashes: {len(interpreter_crashes)}\033[0m")
 print(f"\033[1;33mcompile fails: {len(compile_fails)}\033[0m")
-print(f"\033[1;33minvalid acceptances: {len(accepted_invalid_code)}\033[0m")
 print(f"\033[1;31mincorrect execution: {len(incorrect_execution)}\033[0m")
 print(f"\033[1;32msuccesses: {len(successes)}\033[0m / {total_tests}")
