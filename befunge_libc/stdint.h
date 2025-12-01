1e4dea03cae039ed5f996b7c909433358d763fca
PartyWumpus <48649272+PartyWumpus@users.noreply.github.com>
2025-12-01 00:39:57 +0000

    add stdint.h

---
+++ befunge_libc/stdint.h
@@ -0,0 +1,16 @@
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long uint64_t;
+typedef int int32_t;
+typedef long int64_t;
+#define UINT64_C(c) c ## UL
+#define INT64_C(c) c ## L
+typedef unsigned char uint_least8_t;
+typedef unsigned char uint_fast8_t;
+typedef unsigned short uint_fast16_t;
+typedef unsigned int uint_fast32_t;
+typedef unsigned long uint_fast64_t;
+typedef char int_fast8_t;
+typedef short int_fast16_t;
+typedef int int_fast32_t;
+typedef long int_fast64_t;
