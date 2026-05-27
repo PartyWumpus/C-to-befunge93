0eb3e4bb5d9de2515e66c50f4709a6763d85b869
PartyWumpus <48649272+PartyWumpus@users.noreply.github.com>
2026-05-27 17:42:24 +0100

    add atoi, memset & strlen

--- befunge_libc/stdlib/stdlib.c
+++ befunge_libc/stdlib/stdlib.c
@@ -3,6 +3,46 @@ void exit(int status) {
   asm("55+ , 20g . @" : : ["r20" (status)]);
 }
 
+int atoi(char *str) {
+  int out = 0;
+  int sign = 1;
+
+  while (*str == ' ' || *str == '\t' || *str == '\n') {
+    str++;
+  }
+
+  if (*str == '-') {
+    sign = -1;
+    str++;
+  } else if (*str == '+') {
+    str++;
+  }
+
+  while (*str >= '0' && *str <= '9') {
+    out *= 10;
+    out += (*str - '0');
+    str++;
+  }
+
+  return sign * out;
+}
+
+int strlen(char *str) {
+    char *p = str;
+    while (*p != '\0')
+        p++;
+    return p - str;
+}
+
+void *memset(void *ptr, int c, int n) {
+  unsigned char val = c;
+  unsigned char *p = ptr;
+  while (n--) {
+    *(p++) = val;
+  }
+  return ptr;
+}
+
 // dumb stupid idiot malloc
 void *malloc(int size) {
   static int position = '!';
--- befunge_libc/stdlib/stdlib.h
+++ befunge_libc/stdlib/stdlib.h
@@ -1,4 +1,7 @@
 void exit(int status);
+int atoi(char *str);
+int strlen(char *str);
+void memset(void *ptr, int c, int n);
 void *malloc(int size);
 void *calloc(int num, int size);
 void free(void *ptr);
